2d517c092233e1f36f6822b15175a1d6f1ceba6f
bkhoshbin <bkhoshbin@fsu.edu>
2023-03-07 18:40:57 -0500

    requirements added and mrc file updated

--- magellonService/pyami/mrc.py
+++ magellonService/pyami/mrc.py
@@ -70,7 +70,7 @@ mrcmaptype = {
 numpy2mrc = {
 	## convert these to int8
 	numpy.int8: 0,
-	numpy.bool: 0,
+	bool: 0,
 	numpy.bool_: 0,
 
 	## convert these to int16
@@ -82,12 +82,12 @@ numpy2mrc = {
 	numpy.float64: 2,
 	numpy.int32: 2,
 	numpy.int64: 2,
-	numpy.int: 2,
+	int: 2,
 	numpy.uint32: 2,
 	numpy.uint64: 2,
 
 	## convert these to complex64
-	numpy.complex: 4,
+	complex: 4,
 	numpy.complex64: 4,
 	numpy.complex128: 4,
 
