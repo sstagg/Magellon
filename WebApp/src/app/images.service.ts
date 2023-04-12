e34d4cd52bde33e40621ebe1860aac3961b8db3a
Rupali Garje <rg21w@fsu.edu>
2023-04-11 21:43:17 -0400

    api paths updated in angular and python

--- WebApp/src/app/images.service.ts
+++ WebApp/src/app/images.service.ts
@@ -6,32 +6,32 @@ import { HttpClient } from '@angular/common/http';
 })
 export class ImagesService {
 
-  baseUrl: string = "http://127.0.0.1:5000/"
+  baseUrl: string = "http://127.0.0.1:8000/"
 
   constructor(private httpClient: HttpClient) { }
 
   public getAllImages() {
-    return this.httpClient.get(this.baseUrl + 'get_images'
+    return this.httpClient.get(this.baseUrl + 'web/images'
     );
   }
 
   public getImageByThumbnail(name: string) {
-    return this.httpClient.get(this.baseUrl + 'get_image_by_thumbnail' + '?name=' + name, {
+    return this.httpClient.get(this.baseUrl + 'web/image_by_thumbnail' + '?name=' + name, {
       responseType: 'blob'
     });
   }
 
   public getImagesByStack(ext: string) {
-    return this.httpClient.get(this.baseUrl + 'get_images_by_stack' + '?ext=' + ext);
+    return this.httpClient.get(this.baseUrl + 'web/images_by_stack' + '?ext=' + ext);
   }
 
   public getFFTImageByName(name: string) {
-    return this.httpClient.get(this.baseUrl + 'get_fft_image' + '?name=' + name, {
+    return this.httpClient.get(this.baseUrl + 'web/fft_image' + '?name=' + name, {
       responseType: 'blob'
     });
   }
 
   public getImageDataByName(name: string) {
-    return this.httpClient.get(this.baseUrl + 'get_image_data' + '?name=' + name);
+    return this.httpClient.get(this.baseUrl + 'web/image_data' + '?name=' + name);
   }
 }
