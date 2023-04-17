cf5a5b5bb7184ce488c7ac06c79d6299d3cc6177
bkhoshbin <bkhoshbin@fsu.edu>
2023-04-17 13:16:37 -0400

    mrc conversion to png refactored a lot

--- CoreService/controllers/image_processing_controller.py
+++ CoreService/controllers/image_processing_controller.py
@@ -6,7 +6,6 @@ from services.mrc_image_service import MrcImageService
 
 image_processing_router = APIRouter()
 
-
 # def get_mrc_service() -> ImageFFTService:
 #     return ImageFFTService()
 
@@ -27,14 +26,30 @@ image_processing_router = APIRouter()
 #     # Return a success message as a response
 #     return {"message": f"FFT computed and saved to {destination_path}"}
 
+mrc_service = MrcImageService()
 
-@image_processing_router.post("/png_of_mrc")
-# async def get_png_of_mrc(input: UploadFile = File(...), output: str = ""):
+
+@image_processing_router.post("/png_of_mrc_dir",
+                              summary="Reads each MRC file from the input directory, converts it to a PNG image, and saves the resulting image to the output directory (if provided). If the out_dir argument is not specified, the PNG files are saved in the same directory as the MRC files. ")
 async def get_png_of_mrc(in_dir: str, out_dir: str = ""):
     try:
-        mrc_service = MrcImageService()
-        # Convert the MRC file to PNG using the MrcService
-        mrc_service.mrc2png(indir=in_dir, outdir=out_dir)
+        # in_dir = "C:/temp/images/"
+        # out_dir = "C:/temp/images/"
+        mrc_service.convert_mrc_dir_to_png(in_dir=in_dir, out_dir=out_dir)
+        return {"message": "MRC files successfully converted to PNG!"}
+
+    except Exception as e:
+        return {"error": str(e)}
+
+
+@image_processing_router.post("/png_of_mrc_file",
+                              summary="gets a mrc file, converts it to a PNG image, and saves the resulting image to the output directory (if provided). If the out_dir argument is not specified, the PNG files are saved in the same directory as the MRC files. ")
+# async def get_png_of_mrc(input: UploadFile = File(...), output: str = ""):
+async def get_png_of_mrc(abs_file_path: str, out_dir: str = ""):
+    try:
+        # out_dir = "C:/temp/images/"
+        mrc_service.convert_mrc_to_png(abs_file_path=abs_file_path, out_dir=out_dir)
+        return {"message": "MRC file successfully converted to PNG!"}
 
     except Exception as e:
         return {"error": str(e)}
