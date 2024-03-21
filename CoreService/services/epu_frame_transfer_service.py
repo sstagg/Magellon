b119a467949ef058a8ab477f8c431ebd1a43e455
bkhoshbin <bkhoshbin@fsu.edu>
2024-03-21 14:17:34 -0400

    Update epu_frame_transfer_service.py

--- CoreService/services/epu_frame_transfer_service.py
+++ CoreService/services/epu_frame_transfer_service.py
@@ -1,12 +1,19 @@
 import os
 
+from fastapi import Depends
+
+from database import get_db
 from models.sqlalchemy_models import Frametransferjob, Frametransferjobitem, Image, Project, Msession, Atlas
 from services.file_service import create_directory
-from config import FFT_SUB_URL, IMAGE_SUB_URL, THUMBNAILS_SUB_URL, ORIGINAL_IMAGES_SUB_URL, FRAMES_SUB_URL,ATLAS_SUB_URL
+from config import FFT_SUB_URL, IMAGE_SUB_URL, THUMBNAILS_SUB_URL, ORIGINAL_IMAGES_SUB_URL, FRAMES_SUB_URL, \
+    ATLAS_SUB_URL
 import logging
-
+import uuid
+from services.mrc_image_service import MrcImageService
+from sqlalchemy.orm import Session
 
 logger = logging.getLogger(__name__)
+
 
 def create_directories(target_dir: str):
     create_directory(target_dir)
@@ -18,17 +25,23 @@ def create_directories(target_dir: str):
     create_directory(os.path.join(target_dir, ATLAS_SUB_URL))
 
 
-def get_information_from_xml_files():
-    db_image_list = []
-    db_image = Image(Oid=uuid.uuid4(), name=filename, magnification=image["mag"],
-                     defocus=image["defocus"], dose=image["calculated_dose"],
-                     pixel_size=image["pixelsize"], binning_x=image["bining_x"],
-                     stage_x=image["stage_x"], stage_y=image["stage_y"],
-                     stage_alpha_tilt=image["stage_alpha_tilt"],
-                     atlas_dimxy=image["dimx"],
-                     atlas_delta_row=image["delta_row"],
-                     atlas_delta_column=image["delta_column"],
-                     binning_y=image["bining_y"],
-                     level=get_image_levels(filename, presets_result["regex_pattern"]),
-                     old_id=image["image_id"], session_id=magellon_session.Oid)
-db_session.bulk_save_objects(db_image_list)
+def get_information_from_xml_files(self, db_session: Session = Depends(get_db)):
+    try:
+        db_image_list = []
+        db_image = Image(Oid=uuid.uuid4(), name=filename, magnification=image["mag"],
+                         defocus=image["defocus"], dose=image["calculated_dose"],
+                         pixel_size=image["pixelsize"], binning_x=image["bining_x"],
+                         stage_x=image["stage_x"], stage_y=image["stage_y"],
+                         stage_alpha_tilt=image["stage_alpha_tilt"],
+                         atlas_dimxy=image["dimx"],
+                         atlas_delta_row=image["delta_row"],
+                         atlas_delta_column=image["delta_column"],
+                         binning_y=image["bining_y"],
+                         level=get_image_levels(filename, presets_result["regex_pattern"]),
+                         old_id=image["image_id"], session_id=magellon_session.Oid)
+        db_session.bulk_save_objects(db_image_list)
+        return {'status': 'success', 'message': 'Task completed successfully.'}
+    except Exception as e:
+        return {'status': 'failure', 'message': f'Task failed with error: {str(e)}'}
+
+
