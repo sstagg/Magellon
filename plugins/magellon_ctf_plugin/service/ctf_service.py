3d49d7a447b45e1ef0dea4e06ef62a1cf369c54c
bkhoshbin <bkhoshbin@fsu.edu>
2024-03-27 16:49:01 -0400

    fixing #1

--- plugins/magellon_ctf_plugin/service/ctf_service.py
+++ plugins/magellon_ctf_plugin/service/ctf_service.py
@@ -1,8 +1,15 @@
+import logging
+import os
+import subprocess
+
 from core.model_dto import CryoEmCtfTaskData
-from utils import buildCtfCommand
+from service.ctfeval import run_ctf_evaluation
+from utils import buildCtfCommand, readLastLine, getFileContents
+
+logger = logging.getLogger(__name__)
 
 
-def execute(params: CryoEmCtfTaskData):
+async def do_ctf(params: CryoEmCtfTaskData):
     try:
         input_string = buildCtfCommand(params)
         logger.info("Input String:\n%s", input_string)
@@ -19,7 +26,7 @@ def execute(params: CryoEmCtfTaskData):
         output = process.stdout
         error_output = process.stderr
         return_code = process.returncode
-        logger.info("output",output)
+        logger.info("output", output)
         logger.info("Return Code: %s", return_code)
         if return_code != 0:
             logger.error("Error output: %s", error_output)
@@ -27,15 +34,21 @@ def execute(params: CryoEmCtfTaskData):
             return {"error_output": error_output}
 
         outputFileName = "".join(params.outputFile.split(".")[:-1])
-        CTFestimationValues=await readLastLine(f'{os.getcwd()}/{outputFileName}.txt')
-        result = run_ctf_evaluation(f'{os.getcwd()}/{params.outputFile}', params.pixelSize, params.sphericalAberration, params.accelerationVoltage,params.maximumResolution,float(CTFestimationValues[1])*1e-3,float(CTFestimationValues[2])*1e-3,params.amplitudeContrast, CTFestimationValues[4],math.radians(float(CTFestimationValues[3])))
+        CTFestimationValues = await readLastLine(f'{os.getcwd()}/{outputFileName}.txt')
+        result = run_ctf_evaluation(f'{os.getcwd()}/{params.outputFile}', params.pixelSize, params.sphericalAberration,
+                                    params.accelerationVoltage, params.maximumResolution,
+                                    float(CTFestimationValues[1]) * 1e-3, float(CTFestimationValues[2]) * 1e-3,
+                                    params.amplitudeContrast, CTFestimationValues[4],
+                                    math.radians(float(CTFestimationValues[3])))
         outputSuccessResult = {
             "status_code": 200,
             "message": "ctf completed successfully",
             "output_txt": await getFileContents(f'{os.getcwd()}/{outputFileName}.txt'),
             "output_avrot": await getFileContents(f'{os.getcwd()}/{outputFileName}_avrot.txt'),
             "ctf_analysis_result": result,
-            "ctf_analysis_images":[f'{os.getcwd()}/{params.outputFile}-plots.png',f'{os.getcwd()}/{params.outputFile}-powerspec.jpg',f'{os.getcwd()}/{params.outputFile}']
+            "ctf_analysis_images": [f'{os.getcwd()}/{params.outputFile}-plots.png',
+                                    f'{os.getcwd()}/{params.outputFile}-powerspec.jpg',
+                                    f'{os.getcwd()}/{params.outputFile}']
         }
         # executeMethodSuccess.inc()
         return {"data": outputSuccessResult}
--- plugins/magellon_ctf_plugin/service/service.py
+++ plugins/magellon_ctf_plugin/service/service.py
@@ -12,6 +12,7 @@ from pydantic import BaseModel
 from core.model_dto import CheckRequirementsResult, TaskDto, FFT_TASK, TaskStatusEnum, PENDING, CryoEmFftTaskDetailDto, \
     RequirementResult, RecuirementResultEnum, CryoEmCtfTaskData
 from core.setup_plugin import check_python_version, check_operating_system, check_requirements_txt
+from service.ctf_service import do_ctf
 from service.fft_service import compute_file_fft
 from utils import buildCtfCommand
 
@@ -29,48 +30,56 @@ class InputParams(BaseModel):
 
 def execute(params: CryoEmCtfTaskData):
     try:
-        input_string = buildCtfCommand(params)
-        logger.info("Input String:\n%s", input_string)
+        # fft_path = os.path.join(out_dir, FFT_SUB_URL,os.path.splitext(os.path.basename(abs_file_path))[0] +FFT_SUFFIX)
+        do_ctf(params)
+        return {"message": "MRC file successfully converted to fft PNG!"}
 
-        process = subprocess.run(
-            input_string,
-            cwd=os.getcwd(),
-            shell=True,
-            stdout=subprocess.PIPE,
-            stderr=subprocess.PIPE,
-            universal_newlines=True
-        )
+    except Exception as e:
+        return {"error": str(e)}
 
-        output = process.stdout
-        error_output = process.stderr
-        return_code = process.returncode
-        logger.info("output",output)
-        logger.info("Return Code: %s", return_code)
-        if return_code != 0:
-            logger.error("Error output: %s", error_output)
-            # executeMethodFailure.inc()
-            return {"error_output": error_output}
-
-        outputFileName = "".join(params.outputFile.split(".")[:-1])
-        CTFestimationValues=await readLastLine(f'{os.getcwd()}/{outputFileName}.txt')
-        result = run_ctf_evaluation(f'{os.getcwd()}/{params.outputFile}', params.pixelSize, params.sphericalAberration, params.accelerationVoltage,params.maximumResolution,float(CTFestimationValues[1])*1e-3,float(CTFestimationValues[2])*1e-3,params.amplitudeContrast, CTFestimationValues[4],math.radians(float(CTFestimationValues[3])))
-        outputSuccessResult = {
-            "status_code": 200,
-            "message": "ctf completed successfully",
-            "output_txt": await getFileContents(f'{os.getcwd()}/{outputFileName}.txt'),
-            "output_avrot": await getFileContents(f'{os.getcwd()}/{outputFileName}_avrot.txt'),
-            "ctf_analysis_result": result,
-            "ctf_analysis_images":[f'{os.getcwd()}/{params.outputFile}-plots.png',f'{os.getcwd()}/{params.outputFile}-powerspec.jpg',f'{os.getcwd()}/{params.outputFile}']
-        }
-        # executeMethodSuccess.inc()
-        return {"data": outputSuccessResult}
-
-    except subprocess.CalledProcessError as e:
-        error_message = f"An error occurred: {str(e)}"
-        outputErrorResult = {
-            "status_code": 500,
-            "error_message": error_message
-        }
+    # try:
+    #     input_string = buildCtfCommand(params)
+    #     logger.info("Input String:\n%s", input_string)
+    #
+    #     process = subprocess.run(
+    #         input_string,
+    #         cwd=os.getcwd(),
+    #         shell=True,
+    #         stdout=subprocess.PIPE,
+    #         stderr=subprocess.PIPE,
+    #         universal_newlines=True
+    #     )
+    #
+    #     output = process.stdout
+    #     error_output = process.stderr
+    #     return_code = process.returncode
+    #     logger.info("output",output)
+    #     logger.info("Return Code: %s", return_code)
+    #     if return_code != 0:
+    #         logger.error("Error output: %s", error_output)
+    #         # executeMethodFailure.inc()
+    #         return {"error_output": error_output}
+    #
+    #     outputFileName = "".join(params.outputFile.split(".")[:-1])
+    #     CTFestimationValues=await readLastLine(f'{os.getcwd()}/{outputFileName}.txt')
+    #     result = run_ctf_evaluation(f'{os.getcwd()}/{params.outputFile}', params.pixelSize, params.sphericalAberration, params.accelerationVoltage,params.maximumResolution,float(CTFestimationValues[1])*1e-3,float(CTFestimationValues[2])*1e-3,params.amplitudeContrast, CTFestimationValues[4],math.radians(float(CTFestimationValues[3])))
+    #     outputSuccessResult = {
+    #         "status_code": 200,
+    #         "message": "ctf completed successfully",
+    #         "output_txt": await getFileContents(f'{os.getcwd()}/{outputFileName}.txt'),
+    #         "output_avrot": await getFileContents(f'{os.getcwd()}/{outputFileName}_avrot.txt'),
+    #         "ctf_analysis_result": result,
+    #         "ctf_analysis_images":[f'{os.getcwd()}/{params.outputFile}-plots.png',f'{os.getcwd()}/{params.outputFile}-powerspec.jpg',f'{os.getcwd()}/{params.outputFile}']
+    #     }
+    #     # executeMethodSuccess.inc()
+    #     return {"data": outputSuccessResult}
+    #
+    # except subprocess.CalledProcessError as e:
+    #     error_message = f"An error occurred: {str(e)}"
+    #     outputErrorResult = {
+    #         "status_code": 500,
+    #         "error_message": error_message
+    #     }
         # executeMethodFailure.inc()
 
     # return {"error_output":  outputErrorResult}
