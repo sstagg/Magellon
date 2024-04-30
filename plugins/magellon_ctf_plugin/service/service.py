5b7b9fec33d553b791b1169bd37dcd6a979e3d73
bkhoshbin <bkhoshbin@fsu.edu>
2024-04-30 09:45:28 -0400

    Update service.py

--- plugins/magellon_ctf_plugin/service/service.py
+++ plugins/magellon_ctf_plugin/service/service.py
@@ -1,7 +1,7 @@
 import logging
 
 from core.helper import push_result_to_out_queue
-from core.model_dto import CryoEmCtfTaskData, TaskDto, PluginInfoSingleton
+from core.model_dto import TaskDto, PluginInfoSingleton
 from core.setup_plugin import check_python_version, check_operating_system, check_requirements_txt
 from service.ctf_service import do_ctf
 
@@ -21,27 +21,16 @@ def get_plugin_info():
     return PluginInfoSingleton.get_instance(**plugin_info_data)
 
 
-# async def do_execute_task(task_object: TaskDto):
-#     try:
-#         the_data = CryoEmCtfTaskData.model_validate(task_object.data)
-#         await do_execute(the_data)
-#         return {"message": "MRC file successfully converted to fft PNG!"}
-#     except Exception as exc:
-#         return {"error": str(exc)}
-
-
 async def do_execute(params: TaskDto):
     try:
         # the_data = CryoEmCtfTaskData.model_validate(params.data)
         result = await do_ctf(params)
         if result is not None:
             push_result_to_out_queue(result)
-        # logger("success")
-
+        #     compute_file_fft(mrc_abs_path=request.image_path, abs_out_file_name=request.target_path)
         return {"message": "CTF successfully executed"}
     except Exception as exc:
         return {"error": str(exc)}
-    #     compute_file_fft(mrc_abs_path=request.image_path, abs_out_file_name=request.target_path)
 
 
 async def check_requirements():
@@ -52,4 +41,3 @@ async def check_requirements():
     all_results.extend(await check_requirements_txt())
     # Add more checks for other requirements here
     return all_results
-
