dd4ff3ac643c912d6801f8a122aba684dde1c953
bkhoshbin <bkhoshbin@fsu.edu>
2024-05-02 11:33:24 -0400

    format ctf

--- plugins/magellon_ctf_plugin/service/ctf_service.py
+++ plugins/magellon_ctf_plugin/service/ctf_service.py
@@ -3,24 +3,27 @@ import math
 import os
 import subprocess
 from datetime import datetime
-from core.helper import create_directory
-from core.model_dto import CryoEmCtfTaskData, OutputFile, TaskDto, CryoEmTaskResultDto,ImageMetaData
+
+from core.model_dto import CryoEmCtfTaskData, OutputFile, TaskDto, CryoEmTaskResultDto, ImageMetaData
 from service.ctfeval import run_ctf_evaluation
 from utils import buildCtfCommand, readLastLine, getFileContents
+
 logger = logging.getLogger(__name__)
 
 
 async def do_ctf(the_task: TaskDto) -> CryoEmTaskResultDto:
     try:
-        logger.info(the_task.id)
-        os.makedirs(f'{os.path.join(os.getcwd(), "outputs")}',exist_ok=True)
-        directory_path = os.path.join(os.getcwd(),"outputs", the_task.id)
-        the_task.data["outputFile"]=f'{directory_path}/{the_task.data["outputFile"]}'
-        os.makedirs(directory_path,exist_ok=True)
-        # create_directory(directory_path)
+        logger.info(f"Starting task {the_task.id} ")
         the_task_data = CryoEmCtfTaskData.model_validate(the_task.data)
+        os.makedirs(f'{os.path.join(os.getcwd(), "outputs")}', exist_ok=True)
+        directory_path = os.path.join(os.getcwd(), "outputs", the_task.id)
+        # the_task_data.outputFile = f'{directory_path}/{the_task.data["outputFile"]}'
+        the_task.data["outputFile"] = f'{directory_path}/{the_task.data["outputFile"]}'
+        os.makedirs(directory_path, exist_ok=True)
+        # create_directory(directory_path)
+
         input_string = buildCtfCommand(the_task_data)
-        logger.info("Input String:\n%s", input_string)
+        logger.info("Input String:%s", input_string)
         process = subprocess.run(
             input_string,
             cwd=os.getcwd(),
@@ -41,41 +44,45 @@ async def do_ctf(the_task: TaskDto) -> CryoEmTaskResultDto:
             return {"error_output": error_output}
 
         outputFileName = "".join(the_task_data.outputFile.split(".")[:-1])
+
         CTFestimationValues = await readLastLine(f'{outputFileName}.txt')
-        result = run_ctf_evaluation(f'{the_task_data.outputFile}', the_task_data.pixelSize, the_task_data.sphericalAberration,
+        result = run_ctf_evaluation(f'{the_task_data.outputFile}', the_task_data.pixelSize,
+                                    the_task_data.sphericalAberration,
                                     the_task_data.accelerationVoltage, the_task_data.maximumResolution,
                                     float(CTFestimationValues[1]) * 1e-3, float(CTFestimationValues[2]) * 1e-3,
                                     the_task_data.amplitudeContrast, CTFestimationValues[4],
                                     math.radians(float(CTFestimationValues[3])))
         metaDataList = []
+
         for key, value in result.items():
             metaDataList.append(ImageMetaData(key=key, value=str(value)))
+
         outputSuccessResult = CryoEmTaskResultDto(
-    worker_instance_id=the_task.worker_instance_id,
-    task_id=str(the_task.job_id),
-    image_id=the_task.data["image_id"],
-    image_path=the_task.data["image_path"],
-    code=200,
-    message="ctf executed successfully",
-    description="output for ctf estimation and evaluation for a input file",
-    status=the_task.status,
-    type=the_task.type,
-    created_date=datetime.now(),
-    started_on=the_task.start_on,
-    ended_on=datetime.now(),
-    output_data={
-        "output_txt": await getFileContents(f"{outputFileName}.txt"),
-        "output_avrot": await getFileContents(f"{outputFileName}_avrot.txt")
-    },
-    meta_data=metaDataList,
-    output_files = [
-    OutputFile(name="ctfevalplots", path=f"{outputFileName}.mrc-plots.png", required=True),
-    OutputFile(name="ctfevalpowerspec", path=f"{outputFileName}.mrc-powerspec.jpg", required=True),
-    OutputFile(name="ctfestimationOutputFile", path=f"{outputFileName}.mrc", required=True),
-    OutputFile(name="ctfestimationOutputTextFile", path=f"{outputFileName}.txt", required=True),
-    OutputFile(name="ctfestimationOutputAvrotFile", path=f"{outputFileName}_avrot.txt", required=True),
-]
-) 
+            worker_instance_id=the_task.worker_instance_id,
+            task_id=str(the_task.job_id),
+            image_id=the_task.data["image_id"],
+            image_path=the_task.data["image_path"],
+            code=200,
+            message="ctf executed successfully",
+            description="output for ctf estimation and evaluation for a input file",
+            status=the_task.status,
+            type=the_task.type,
+            created_date=datetime.now(),
+            started_on=the_task.start_on,
+            ended_on=datetime.now(),
+            output_data={
+                "output_txt": await getFileContents(f"{outputFileName}.txt"),
+                "output_avrot": await getFileContents(f"{outputFileName}_avrot.txt")
+            },
+            meta_data=metaDataList,
+            output_files=[
+                OutputFile(name="ctfevalplots", path=f"{outputFileName}.mrc-plots.png", required=True),
+                OutputFile(name="ctfevalpowerspec", path=f"{outputFileName}.mrc-powerspec.jpg", required=True),
+                OutputFile(name="ctfestimationOutputFile", path=f"{outputFileName}.mrc", required=True),
+                OutputFile(name="ctfestimationOutputTextFile", path=f"{outputFileName}.txt", required=True),
+                OutputFile(name="ctfestimationOutputAvrotFile", path=f"{outputFileName}_avrot.txt", required=True),
+            ]
+        )
         # executeMethodSuccess.inc()
         return {"data": outputSuccessResult}
 
