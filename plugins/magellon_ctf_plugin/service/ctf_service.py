e0bcd337ad68012f61aa828e54afe8ec70fd0967
MDPuneethReddy <64721445+MDPuneethReddy@users.noreply.github.com>
2024-09-03 13:45:35 -0400

    update ctf service

--- plugins/magellon_ctf_plugin/service/ctf_service.py
+++ plugins/magellon_ctf_plugin/service/ctf_service.py
@@ -22,45 +22,33 @@ async def do_ctf(the_task: TaskDto) -> TaskResultDto:
         d.line1 = the_task_data.inputFile
         d.line2 = the_task_data.image_path
 
-        if AppSettingsSingleton.get_instance().REPLACE_TYPE == "standard":
+        replace_settings = AppSettingsSingleton.get_instance()
+        if replace_settings.REPLACE_TYPE == "standard":
             the_task_data.inputFile = the_task_data.inputFile.replace(
-                AppSettingsSingleton.get_instance().REPLACE_PATTERN, AppSettingsSingleton.get_instance().REPLACE_WITH)
+                replace_settings.REPLACE_PATTERN, replace_settings.REPLACE_WITH
+            )
             the_task_data.image_path = the_task_data.image_path.replace(
-                AppSettingsSingleton.get_instance().REPLACE_PATTERN, AppSettingsSingleton.get_instance().REPLACE_WITH)
+                replace_settings.REPLACE_PATTERN, replace_settings.REPLACE_WITH
+            )
 
         d.line3 = the_task_data.inputFile
         d.line4 = the_task_data.image_path
 
-        # transformed_path = the_task_data.inputFile.replace(r"C:\temp\magellon", "/gpfs")
-        # d.line7 = transformed_path
-        # Normalize the path to ensure consistency
-        final_path = os.path.normpath(the_task_data.inputFile)
-        # Replace backslashes with forward slashes for Unix-style paths
-        final_path = final_path.replace("\\", "/")
+        final_path = os.path.normpath(the_task_data.inputFile).replace("\\", "/")
         the_task_data.inputFile = final_path
         the_task_data.image_path = final_path
 
         d.line5 = the_task_data.inputFile
         d.line6 = the_task_data.image_path
-
         push_info_to_debug_queue(d)
-        # os.makedirs(f'{os.path.join(os.getcwd(),"gpfs", "outputs")}', exist_ok=True)
-        # directory_path = os.path.join(os.getcwd(),"gpfs", "outputs", the_task.id)
-
-        host_dir_path = os.path.join(AppSettingsSingleton.get_instance().HOST_JOBS_DIR, str(the_task.id))
-        host_file_path = os.path.join(host_dir_path, the_task.data["outputFile"])
 
         directory_path = os.path.join(AppSettingsSingleton.get_instance().JOBS_DIR, str(the_task.id))
-        the_task_data.outputFile = os.path.join(directory_path, the_task.data["outputFile"])
-        host_output_file_name = "".join(host_file_path.split(".")[:-1])
-        # print(directory_path)
         os.makedirs(directory_path, exist_ok=True)
-
-        #testing if really we have access to input files
-        # shutil.copy2(the_task_data.inputFile, directory_path)
+        host_file_path = os.path.join(AppSettingsSingleton.get_instance().HOST_JOBS_DIR, str(the_task.id), the_task.data["outputFile"])
+        the_task_data.outputFile = os.path.join(directory_path, the_task.data["outputFile"])
 
         input_string = buildCtfCommand(the_task_data)
-        logger.info("Input String:%s", input_string)
+        logger.info("Input String: %s", input_string)
         process = subprocess.run(
             input_string,
             cwd=os.getcwd(),
@@ -75,101 +63,97 @@ async def do_ctf(the_task: TaskDto) -> TaskResultDto:
         output = process.stdout.strip()
         error_output = process.stderr.strip()
         return_code = process.returncode
+
         if return_code != 0:
-            # logger.error("Standard output: %s", output)
-            # logger.error("Error output: %s", error_output)
-            raise Exception(f"Command failed with return code {process.returncode}:{output} {error_output}")
-            # executeMethodFailure.inc()
-            return {"error_output": error_output}
+            raise Exception(f"Command failed with return code {process.returncode}: {output} {error_output}")
+
         outputFileName = "".join(the_task_data.outputFile.split(".")[:-1])
         CTFestimationValues = await readLastLine(f'{outputFileName}.txt')
-        report_file_name= outputFileName # host_output_file_name
-        result = await run_ctf_evaluation(f'{the_task_data.outputFile}', the_task_data.pixelSize,
-                                          the_task_data.sphericalAberration,
-                                          the_task_data.accelerationVoltage, the_task_data.maximumResolution,
-                                          float(CTFestimationValues[1]) * 1e-3, float(CTFestimationValues[2]) * 1e-3,
-                                          the_task_data.amplitudeContrast, CTFestimationValues[4],
-                                          math.radians(float(CTFestimationValues[3])))
+
+        # Initialize metadata list
         metaDataList = []
 
-        for key, value in result.items():
-            metaDataList.append(ImageMetaData(key=key, value=str(value)))
+        try:
+            result = await run_ctf_evaluation(
+                f'{the_task_data.outputFile}', the_task_data.pixelSize, the_task_data.sphericalAberration,
+                the_task_data.accelerationVoltage, the_task_data.maximumResolution,
+                float(CTFestimationValues[1]) * 1e-3, float(CTFestimationValues[2]) * 1e-3,
+                the_task_data.amplitudeContrast, CTFestimationValues[4],
+                math.radians(float(CTFestimationValues[3]))
+            )
 
-        outputSuccessResult = TaskResultDto(
-            worker_instance_id=the_task.worker_instance_id,
-            task_id=the_task.id,
-            job_id=the_task.job_id,  #  str(the_task.job_id),
-            image_id=the_task.data["image_id"],
-            image_path=the_task.data["image_path"],
-            session_name=the_task.sesson_name,
-            code=200,
-            message="ctf executed successfully",
-            description="output for ctf estimation and evaluation for a input file",
-            status=the_task.status,
-            type=the_task.type,
-            created_date=datetime.now(),
-            started_on=the_task.start_on,
-            ended_on=datetime.now(),
-            output_data={
-                "output_txt": await getFileContents(f"{outputFileName}.txt"),
-                "output_avrot": await getFileContents(f"{outputFileName}_avrot.txt")
-            },
-            meta_data=metaDataList,
-            output_files=[
-                OutputFile(name="ctfevalplots", path=f"{report_file_name}.mrc-plots.png", required=True),
-                OutputFile(name="ctfevalpowerspec", path=f"{report_file_name}.mrc-powerspec.jpg", required=True),
-                OutputFile(name="ctfestimationOutputFile", path=f"{report_file_name}.mrc", required=True),
-                OutputFile(name="ctfestimationOutputTextFile", path=f"{report_file_name}.txt", required=True),
-                OutputFile(name="ctfestimationOutputAvrotFile", path=f"{report_file_name}_avrot.txt",
-                           required=True),
-            ]
-        )
-        # executeMethodSuccess.inc()
-        return outputSuccessResult
+            metaDataList = [ImageMetaData(key=key, value=str(value)) for key, value in result.items()]
 
-    except subprocess.CalledProcessError as e:
-        error_message = f"Command '{e.cmd}' failed with return code {e.returncode}: {e.stderr}"
-        # return {
-        #     "status_code": 500,
-        #     "error_message": error_message
-        # }
-        return TaskResultDto(
-            worker_instance_id=the_task.worker_instance_id,
-            task_id=the_task.id,
-            job_id=the_task.job_id,  #  str(the_task.job_id),
-            image_id=the_task.data["image_id"],
-            image_path=the_task.data["image_path"],
-            session_name=the_task.sesson_name,
-            code=500,
-            message="ctf execution is unsuccessfull",
-            description=error_message,
-            status=the_task.status,
-            type=the_task.type,
-            created_date=datetime.now(),
-            started_on=the_task.start_on,
-            ended_on=datetime.now(),
-            output_data={},
-            meta_data=[],
-            output_files=[]
-        )
+            return TaskResultDto(
+                worker_instance_id=the_task.worker_instance_id,
+                task_id=the_task.id,
+                job_id=the_task.job_id,
+                image_id=the_task.data["image_id"],
+                image_path=the_task.data["image_path"],
+                session_name=the_task.sesson_name,
+                code=200,
+                message="CTF executed successfully",
+                description="Output for CTF estimation and evaluation for an input file",
+                status=the_task.status,
+                type=the_task.type,
+                created_date=datetime.now(),
+                started_on=the_task.start_on,
+                ended_on=datetime.now(),
+                output_data={
+                    "output_txt": await getFileContents(f"{outputFileName}.txt"),
+                    "output_avrot": await getFileContents(f"{outputFileName}_avrot.txt")
+                },
+                meta_data=metaDataList,
+                output_files=[
+                    OutputFile(name="ctfevalplots", path=f"{outputFileName}.mrc-plots.png", required=True),
+                    OutputFile(name="ctfevalpowerspec", path=f"{outputFileName}.mrc-powerspec.jpg", required=True),
+                    OutputFile(name="ctfestimationOutputFile", path=f"{outputFileName}.mrc", required=True),
+                    OutputFile(name="ctfestimationOutputTextFile", path=f"{outputFileName}.txt", required=True),
+                    OutputFile(name="ctfestimationOutputAvrotFile", path=f"{outputFileName}_avrot.txt", required=True),
+                ]
+            )
+
+        except Exception as eval_error:
+            logger.warning(f"CTF evaluation failed: {str(eval_error)}")
+            return TaskResultDto(
+                worker_instance_id=the_task.worker_instance_id,
+                task_id=the_task.id,
+                job_id=the_task.job_id,
+                image_id=the_task.data["image_id"],
+                image_path=the_task.data["image_path"],
+                session_name=the_task.sesson_name,
+                code=200,
+                message="CTF executed successfully but evaluation failed",
+                description="Output for CTF estimation and evaluation for an input file",
+                status=the_task.status,
+                type=the_task.type,
+                created_date=datetime.now(),
+                started_on=the_task.start_on,
+                ended_on=datetime.now(),
+                output_data={
+                    "output_txt": await getFileContents(f"{outputFileName}.txt"),
+                    "output_avrot": await getFileContents(f"{outputFileName}_avrot.txt")
+                },
+                meta_data=[],
+                output_files=[
+                    OutputFile(name="ctfestimationOutputFile", path=f"{outputFileName}.mrc", required=True),
+                    OutputFile(name="ctfestimationOutputTextFile", path=f"{outputFileName}.txt", required=True),
+                    OutputFile(name="ctfestimationOutputAvrotFile", path=f"{outputFileName}_avrot.txt", required=True),
+                ]
+            )
 
     except Exception as e:
-        error_message = f"An error occurred: {str(e)}"
-        # logger.error(error_message)
-        # return {
-        #     "status_code": 500,
-        #     "error_message": error_message
-        # }
+        logger.error(f"An error occurred: {str(e)}")
         return TaskResultDto(
             worker_instance_id=the_task.worker_instance_id,
             task_id=the_task.id,
-            job_id=the_task.job_id,  #  str(the_task.job_id),
+            job_id=the_task.job_id,
             image_id=the_task.data["image_id"],
             image_path=the_task.data["image_path"],
             session_name=the_task.sesson_name,
             code=500,
-            message="ctf execution is unsuccessfull",
-            description=error_message,
+            message="CTF execution was unsuccessful",
+            description=f"An error occurred: {str(e)}",
             status=the_task.status,
             type=the_task.type,
             created_date=datetime.now(),
