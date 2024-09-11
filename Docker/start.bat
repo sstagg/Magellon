b00e44cfd357739f58cd39e4645f21ffbe7b4464
bkhoshbin <bkhoshbin@fsu.edu>
2024-09-11 13:53:51 -0400

    enhancing docker script

--- Docker/start.bat
+++ Docker/start.bat
@@ -1,12 +1,16 @@
 @echo off
 
 REM Step 1: Start Docker Compose
+REM docker-compose  up -d
 docker-compose --profile default up -d
 
 REM Step 2: Wait for services to be ready
 timeout /t 15
 
 REM Step 3: Open browser with links
-start chrome "http://localhost:8080"
+start chrome "http://localhost:8080/en/panel/images"
 start chrome "http://localhost:8000"
 start chrome "http://localhost:15672"
+start chrome "http://localhost:8500"
+REM start chrome "http://localhost:3000"
+REM start chrome "http://localhost:9090"
--- Docker/start.sh
+++ Docker/start.sh
@@ -7,21 +7,32 @@ docker-compose up -d
 # You can add a sleep here to give the services some time to start, or use a more sophisticated health check
 sleep 15
 
+
+open_urls() {
+  local urls=(
+    "http://localhost:8080/en/panel/images"
+    "http://localhost:8000"
+    "http://localhost:15672"
+    "http://localhost:8500"
+    "http://localhost:3000"
+    "http://localhost:9090"
+  )
+
 # Step 3: Open browser with links
 # Replace the URL with the desired URL for your service
 if which xdg-open > /dev/null
 then
-  xdg-open "http://localhost:8080"
+  xdg-open "http://localhost:8080/en/panel/images"
   xdg-open "http://localhost:8000"
   xdg-open "http://localhost:15672"
 elif which gnome-open > /dev/null
 then
-  gnome-open "http://localhost:8080"
+  gnome-open "http://localhost:8080/en/panel/images"
   gnome-open "http://localhost:8000"
   gnome-open "http://localhost:15672"
 elif which open > /dev/null    # For macOS
 then
-  open "http://localhost:8080"
+  open "http://localhost:8080/en/panel/images"
   open "http://localhost:8000"
   open "http://localhost:15672"
 fi
