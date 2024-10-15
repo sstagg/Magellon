cc7ff504eefe38394b619f6fed834cd06c4b40df
bkhoshbin <bkhoshbin@fsu.edu>
2024-10-15 14:59:20 -0400

    Update EPUImporter.py

--- CoreService/services/importers/EPUImporter.py
+++ CoreService/services/importers/EPUImporter.py
@@ -35,63 +35,40 @@ namespaces = {
     'ns5': 'http://schemas.datacontract.org/2004/07/System.Drawing'
 }
 
-fields = {
-    'uniqueID': None,
-    'DoseOnCamera': None,
-    'Dose': None,
-    'Defocus': None,
-    'dimension_x': None,
-    'Binning/ns5:x': None,
-    'Binning/ns5:y': None,
-    'pixelSize/ns0:x/ns0:numericValue': None,
-    'atlas_delta_row': None,
-    'atlas_delta_column': None,
-    'stage_alpha_tilt': None,
-    'stage/ns0:Position/ns0:X': None,
-    'stage/ns0:Position/ns0:Y': None,
-}
-# Function to create EPUMetadata from a dictionary
-def create_epu_metadata(fields: dict) -> EPUMetadata:
-    # Create an instance of EPUMetadata using the dictionary values
-    return EPUMetadata(
-        uniqueID=fields.get('uniqueID'),
-        DoseOnCamera=fields.get('DoseOnCamera'),
-        Dose=fields.get('Dose'),
-        Defocus=fields.get('Defocus'),
-        dimension_x=fields.get('dimension_x'),
-        binning_x=fields.get('Binning/ns5:x'),
-        binning_y=fields.get('Binning/ns5:y'),
-        pixelSize_x=fields.get('pixelSize/ns0:x/ns0:numericValue'),
-        atlas_delta_row=fields.get('atlas_delta_row'),
-        atlas_delta_column=fields.get('atlas_delta_column'),
-        stage_alpha_tilt=fields.get('stage_alpha_tilt'),
-        stage_position_x=fields.get('stage/ns0:Position/ns0:X'),
-        stage_position_y=fields.get('stage/ns0:Position/ns0:Y')
-    )
 
-
-def parse_xml(xml_content: bytes) -> Dict[str, Any]:
+def parse_xml(xml_content: bytes) -> EPUMetadata:
     root = etree.parse(BytesIO(xml_content))
-    results = fields.copy()
+    metadata = {}
 
-    for e in root.iter():
-        path = root.getelementpath(e)
+    xpath_mapping = {
+        'uniqueID': './/ns0:uniqueID',
+        'DoseOnCamera': './/ns0:DoseOnCamera',
+        'Dose': './/ns0:Dose',
+        'Defocus': './/ns0:Defocus',
+        'dimension_x': './/ns0:dimension/ns0:width',
+        'binning_x': './/ns0:Binning/ns5:x',
+        'binning_y': './/ns0:Binning/ns5:y',
+        'pixelSize_x': './/ns0:pixelSize/ns0:x/ns0:numericValue',
+        'atlas_delta_row': './/ns0:atlas_delta_row',
+        'atlas_delta_column': './/ns0:atlas_delta_column',
+        'stage_alpha_tilt': './/ns0:stage_alpha_tilt',
+        'stage_position_x': './/ns0:stage/ns0:Position/ns0:X',
+        'stage_position_y': './/ns0:stage/ns0:Position/ns0:Y'
+    }
 
-        for n in namespaces:
-            path = path.replace('{'+namespaces[n]+'}', n+':')
+    for field, xpath in xpath_mapping.items():
+        element = root.find(xpath, namespaces)
+        if element is not None:
+            value = element.text
+            # Convert to appropriate type
+            if field in ['dimension_x', 'binning_x', 'binning_y']:
+                metadata[field] = int(value) if value else None
+            elif 'position' in field or field in ['DoseOnCamera', 'Dose', 'Defocus', 'pixelSize_x', 'atlas_delta_row', 'atlas_delta_column', 'stage_alpha_tilt']:
+                metadata[field] = float(value) if value else None
+            else:
+                metadata[field] = value
 
-        for field in fields:
-            if path.endswith(':'+field):
-                val = root.find('.//' + path, namespaces)
-                results[field] = val.text if val is not None else None
-            if path.endswith(':Key'):
-                val = root.find('.//' + path, namespaces)
-                if val is not None and val.text in fields:
-                    value_path = path.replace(']/ns1:Key', ']/ns1:Value')
-                    value = root.find('.//' + value_path, namespaces)
-                    results[val.text] = value.text if value is not None else None
-
-    return results
+    return EPUMetadata(**metadata)
 
 
 
@@ -114,4 +91,5 @@ class EPUImporter(BaseImporter):
 
     def parse_epu_xml(self, xml_content: bytes):
         # Use the existing parse_xml function
-        return create_epu_metadata(parse_xml(xml_content))
+        # return create_epu_metadata(parse_xml(xml_content))
+        return parse_xml(xml_content)
