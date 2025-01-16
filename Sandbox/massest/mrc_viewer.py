18287f56136783c8098fb250a0d07a218cfa20c6
Frank Schotanus <frankschotanus@Franks-MacBook-Pro.local>
2025-01-15 21:41:05 -0500

    Updated mrc viewer

--- Sandbox/massest/mrc_viewer.py
+++ Sandbox/massest/mrc_viewer.py
@@ -15,6 +15,7 @@ def parse_json(file_path):
 
     # Extract the first (and only) dictionary inside the JSON
     inner_dict = next(iter(data.values()))
+
     # Extract the integer values in the order they appear
     values = list(inner_dict.values())
 
@@ -25,16 +26,19 @@ meta_data = {}
 
 class ImageGridApp:
     def __init__(self, root, images_data):
+
         self.root = root
         self.images_data = images_data
-        self.const_images_data = images_data
         self.num_images = images_data.shape[0]
         self.height, self.width = images_data[0].shape
 
+        self.data_min = self.images_data[0].min()
+        self.data_max = self.images_data[0].max()
+
         self.scale_factor = 1.0  # Default scale factor
-        self.prev_cols = None  # Track the previous column count
         self.resize_timer = None  # Timer for debounce logic
 
+        #default brightness and contrast
         self.brightness=50
         self.contrast=50
 
@@ -42,13 +46,16 @@ class ImageGridApp:
 
         #how many slices/images should be shown at one time, default=10
         self.items_per_page = 10
-        #probably unnecessarry variable to keep track of items per page
-        #the above ipp variable is subject to change if it goes out of bounds, this one wont change
+
+        #self.items_per_page is subject to change if it goes out of bounds, this one won't change in that situation
         self.const_items_per_page=10
+
         #index of the first slice to be displayed on the current page
         self.first_slice_index=0
+
         #keep track of page number
         self.page_number=1
+
         self.selected_metadata = None
 
 
@@ -59,13 +66,13 @@ class ImageGridApp:
         self.grid_canvas.bind("<Configure>", self.debounce(self.on_canvas_resize, 500))
         self.grid_canvas.bind_all("<MouseWheel>", self.on_mouse_scroll)  # Windows & Linux
         self.grid_canvas.bind_all("<Button-4>", self.on_mouse_scroll)  # macOS (up)
-        self.grid_canvas.bind_all("<Button-5>", self.on_mouse_scroll)  # macOS (down
+        self.grid_canvas.bind_all("<Button-5>", self.on_mouse_scroll)  # macOS (down)
 
         # Initial grid update
         self.update_grid()
 
     def debounce(self, func, delay):
-        """Debounce function to limit how often 'func' is called."""
+        """Debounce function to limit how often 'update_grid' is called."""
         def wrapper(*args, **kwargs):
             if self.resize_timer:
                 self.root.after_cancel(self.resize_timer)
@@ -100,6 +107,7 @@ class ImageGridApp:
         self.scrollbar.pack(side=tk.RIGHT, fill=tk.Y)
 
     def update_info_grid(self, frame):
+        """Update the info box with page and slice numbers"""
         for widget in frame.winfo_children():
             widget.destroy()
         page_label = tk.Label(frame,
@@ -109,6 +117,7 @@ class ImageGridApp:
 
     def open_controls_panel(self):
         """Open a popup window with the controls."""
+
         # Create the Toplevel window (popup)
         control_window = tk.Toplevel(self.root)
         control_window.title("Settings")
@@ -128,29 +137,31 @@ class ImageGridApp:
             "50",
             "100"
         ])
+
+        #items per page combobox
         self.items_per_page_combobox.grid(row=0, column=1, padx=5, pady=10)
         self.items_per_page_combobox.current(2)
         self.items_per_page_combobox.bind("<<ComboboxSelected>>", self.update_items_per_page)
 
+        #metadata combobox
         self.metadata_label= tk.Label(control_frame, text="Metadata file: ", bg="White")
         self.metadata_label.grid(row=1, padx=5, pady=10)
-
         self.metadata_combobox = ttk.Combobox(control_frame, state="readonly", values=list(self.metadata_files.keys()))
         self.metadata_combobox.grid(row=1, column=1, padx=5, pady=10)
         self.metadata_combobox.set("Select a metadata file")
         self.metadata_combobox.bind("<<ComboboxSelected>>", self.set_metadata)
 
+        #brightness slider
         self.brightness_label = tk.Label(control_frame, text="Brightness: ", bg="White")
         self.brightness_label.grid(row=2, padx=5, pady=10)
-
         self.brightness_slider = tk.Scale(control_frame, from_=0, to=100, orient="horizontal", command=self.change_brightness,
                                           length=200, bg="White")
         self.brightness_slider.set(self.brightness)
         self.brightness_slider.grid(row=2, column=1, padx=5, pady=10)
 
+        #contrast slider
         self.contrast_label = tk.Label(control_frame, text="Contrast: ", bg="White")
         self.contrast_label.grid(row=3, padx=5, pady=10)
-
         self.contrast_slider = tk.Scale(control_frame, from_=0, to=100, orient="horizontal", command=self.change_contrast,
                                         length=200, bg="White")
         self.contrast_slider.set(self.contrast)
@@ -158,7 +169,7 @@ class ImageGridApp:
 
 
     def create_controls(self, frame):
-        """Create input box, buttons, and scale controls."""
+        """Create input box, buttons, and scale controls.(bottom of main window)"""
         # Frame for Left-aligned controls
         left_frame = tk.Frame(frame, bg="White")
         left_frame.pack(side=tk.LEFT, anchor="w")
@@ -166,15 +177,12 @@ class ImageGridApp:
         # Scale controls (Left-aligned)
         scale_label = tk.Label(left_frame, text="Scale:", bg="White", font=('Arial', 11))
         scale_label.pack(side=tk.LEFT, padx=5)
-
         decrease_button = tk.Button(left_frame, text="-", command=lambda: self.adjust_scale(-0.1))
         decrease_button.pack(side=tk.LEFT, padx=5)
-
         self.scale_entry = tk.Entry(left_frame, width=5, font=('Arial', 11))
         self.scale_entry.pack(side=tk.LEFT, padx=5)
         self.scale_entry.insert(0, "1")
         self.scale_entry.bind("<Return>", self.set_scale)
-
         increase_button = tk.Button(left_frame, text="+", command=lambda: self.adjust_scale(0.1))
         increase_button.pack(side=tk.LEFT, padx=5)
 
@@ -185,10 +193,8 @@ class ImageGridApp:
         # Page controls (Center-aligned)
         previous_page_button = tk.Button(center_frame, text="<", command=lambda: self.change_page(-1))
         previous_page_button.pack(side=tk.LEFT, padx=5)
-
         page_label = tk.Label(center_frame, text="Page", bg="White", font=('Arial', 11))
         page_label.pack(side=tk.LEFT, padx=5)
-
         next_page_button = tk.Button(center_frame, text=">", command=lambda: self.change_page(1))
         next_page_button.pack(side=tk.LEFT, padx=5)
 
@@ -196,20 +202,16 @@ class ImageGridApp:
         right_frame = tk.Frame(frame, bg="White")
         right_frame.pack(side=tk.RIGHT, anchor="e")
 
-        # Controls button (Right-aligned)
+        # Control window button (Right-aligned)
         controls_button = tk.Button(right_frame, text="Settings", command=self.open_controls_panel)
         controls_button.pack(pady=10, padx=10)
 
     def change_contrast(self, event):
         self.contrast=int(self.contrast_slider.get())
-        img_float = self.const_images_data.astype(np.float32)
-        adjusted_data = 128 + int(self.contrast_slider.get()) / 50 * (img_float - 128)
-        self.images_data = np.clip(adjusted_data, 0, 510)
         self.update_grid()
 
     def change_brightness(self, event):
         self.brightness = int(self.brightness_slider.get())
-        self.images_data = np.clip(self.const_images_data * int(self.brightness_slider.get())/50, 0, 510)
         self.update_grid()
 
     def set_metadata(self, event):
@@ -218,27 +220,32 @@ class ImageGridApp:
 
     def change_page(self, increment):
         """Logic for changing which images are currently being displayed"""
+        
+        #next page
         if increment == 1:
             if self.first_slice_index+self.items_per_page >= len(images_data):
                 self.items_per_page = len(images_data) - self.first_slice_index
             else:
                 self.first_slice_index+=self.items_per_page
+        #previous page
         elif increment == -1:
             if self.first_slice_index - self.items_per_page >= 0:
                 self.first_slice_index -= self.items_per_page
             else:
                 pass
-                #error message here?
 
         self.update_grid()
 
     def update_items_per_page(self, event):
+        """changes how many images are being shown per page, 
+        when changed it goes back to image #1"""
         self.items_per_page = int(self.items_per_page_combobox.get())
         self.const_items_per_page = int(self.items_per_page_combobox.get())
         self.first_slice_index = 0
         self.update_grid()
 
     def set_scale(self, event):
+        """Change scale of image"""
         try:
             scale = float(self.scale_entry.get())
         except ValueError:
@@ -258,8 +265,7 @@ class ImageGridApp:
             self.grid_canvas.yview_scroll(1, "units")
 
     def update_grid(self):
-        """Update the grid layout with the current scale factor."""
-
+        """Update the grid layout. Called very frequently"""
         #scale width and height
         scaled_height = int(self.height * self.scale_factor)
         scaled_width = int(self.width * self.scale_factor)
@@ -271,6 +277,8 @@ class ImageGridApp:
         for widget in self.grid_frame.winfo_children():
             widget.destroy()
 
+        #logic for when ipp(items per page) is greater than the number of images
+        #i.e. Setting ipp to 100 but there are only 25 images in the stack
         if self.items_per_page > len(images_data):
             self.items_per_page = len(images_data)
         else:
@@ -280,8 +288,14 @@ class ImageGridApp:
         self.page_number = self.first_slice_index // self.const_items_per_page + 1
         counter = 0
 
+        #loop through the images currently being displayed
         for i in range(self.first_slice_index,min(self.first_slice_index + self.items_per_page, len(images_data))):
-            img = self.images_data[i]
+            
+            #Normalizes the data, and applies current brightness and contrast modifiers
+            img = 128 + self.contrast / 50 * (((self.images_data[i] - self.data_min) / 
+                                               (self.data_max - self.data_min) * (self.brightness * 5.1)).astype(np.float32) - 128)
+
+            #create image object
             pil_image = Image.fromarray(img)
             pil_image = pil_image.resize((scaled_width, scaled_height), Image.LANCZOS)
             photo = ImageTk.PhotoImage(pil_image)
@@ -295,7 +309,7 @@ class ImageGridApp:
             img_label.image = photo  # Keep a reference
             img_label.place(relx=0.5, rely=0.5, anchor=tk.CENTER)
 
-            # Add the number overlay at the center-bottom
+            # Add the metadata overlay at the center-bottom
             self.add_number_overlay(image_frame, str(f"{i+1}"))
             if self.selected_metadata is not None:
                 self.add_number_overlay(image_frame, self.metadata_files[self.selected_metadata][i], position=(.5, 1), pos='s')
@@ -303,7 +317,8 @@ class ImageGridApp:
 
         self.update_info_grid(self.info_frame)
 
-        self.grid_canvas.update_idletasks()  # Ensure the layout refreshes immediately
+        # Ensure the layout refreshes immediately
+        self.grid_canvas.update_idletasks() 
 
     def adjust_scale(self, delta):
         """Adjust the scale factor by the given delta."""
@@ -316,23 +331,22 @@ class ImageGridApp:
         self.scale_entry.delete(0, tk.END)
         self.scale_entry.insert(0, f"{scale:.1f}")
         self.scale_factor = scale  # Update the scale factor
-        self.update_grid()  # Immediately update the grid
+
+        self.update_grid()
 
     def add_number_overlay(self, parent, number, position=(0.05, 0.05), bg="black", fg="white", pos = "nw"):
-        """Overlay a number or text on the parent frame."""
+        """Overlay the slice's number in the stack"""
         label = tk.Label(parent, text=str(number), bg=bg, fg=fg)
         label.place(relx=position[0], rely=position[1], anchor=pos)
 
 def read_images_from_mrc(file_path):
     """Read images from an MRC file."""
     with mrcfile.mmap(file_path, mode='r', permissive=True) as mrc:
-        data = mrc.data
-        data_min = data.min()
-        data_max = data.max()
-        normalized_data = (data - data_min) / (data_max - data_min) * 255#BRIGHTNESS FACTOR
-        return normalized_data
+        return mrc.data
 
 def parseArgs():
+    """parse command line arguments
+    Usage: python mrc_viewer.py --stackpath [filepath to mrc] --metadata [path to metadata file], [path to metadata file]..."""
     parser = argparse.ArgumentParser()
     parser.add_argument("--stackpath", dest="stackpath", type=str, help="Path to stack")
     parser.add_argument('--metadata', nargs='*', type=str, dest="metadata",help='Path to desired metadata files to be overlaid')
@@ -347,9 +361,9 @@ if __name__ == "__main__":
     args = parseArgs()
     mrc_file_path = args.stackpath
     metadata_path = args.metadata
-    for json_file_path in metadata_path:
-        meta_data[json_file_path] = parse_json(json_file_path)
-    mrcfile.validate(mrc_file_path)
+    if metadata_path is not None:
+        for json_file_path in metadata_path:
+            meta_data[json_file_path] = parse_json(json_file_path)
     images_data = read_images_from_mrc(mrc_file_path)
 
     root = tk.Tk()
