44f6e4dc69a95daefb711b1ad1b59433f2521105
bkhoshbin <bkhoshbin@fsu.edu>
2025-05-13 13:18:19 -0400

    pruning fronend

--- magellon-react-app/src/main.tsx
+++ magellon-react-app/src/main.tsx
@@ -1,9 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-// import OldApp from './components/App.tsx'
 import './assets/css/index.css'
-import {store} from "./core/reduxStore.ts";
-import {Provider as ReduxProvider} from 'react-redux';
 import {BrowserRouter} from "react-router-dom";
 import AppRoutes from "./AppRoutes.tsx";
 import './core/i18n';
@@ -13,12 +10,10 @@ const queryClient = new QueryClient()
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
     <React.StrictMode>
-        <ReduxProvider store={store}>
             <QueryClientProvider client={queryClient}>
                 <BrowserRouter>
                         <AppRoutes/>
                 </BrowserRouter>
             </QueryClientProvider>
-        </ReduxProvider>
     </React.StrictMode>,
 )
