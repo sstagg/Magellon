bf86c25f681b84fa14de04836a8e3a433d3674d7
bkhoshbin <122561268+bkhoshbin@users.noreply.github.com>
2026-05-26 11:45:07 -0400

    fix: announce template picker display name

--- plugins/magellon_template_picker_plugin/plugin/plugin.py
+++ plugins/magellon_template_picker_plugin/plugin/plugin.py
@@ -90,7 +90,7 @@ class TemplatePickerPlugin(PluginBase[TemplatePickerInput, ParticlePickingOutput
 
     def get_info(self) -> PluginInfo:
         return PluginInfo(
-            name="template-picker",
+            name="Template Picker",
             version="0.1.0",
             developer="Behdad Khoshbin b.khoshbin@gmail.com",
             description=(
